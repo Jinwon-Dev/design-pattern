824cb477161c87f2b15a88f1c100b42f6f7ca467
Jinwon-Dev <yjw9424@naver.com>
2022-09-24 23:00:36 +0900

    팩토리 패턴 적용하기

--- src/Chapter12/combining/DuckSimulator.java
+++ src/Chapter12/combining/DuckSimulator.java
@@ -3,17 +3,19 @@ package Chapter12.combining;
 public class DuckSimulator {
     public static void main(String[] args) {
         DuckSimulator simulator = new DuckSimulator();
-        simulator.simulate();
+        AbstractDuckFactory duckFactory = new CountingDuckFactory();
+
+        simulator.simulate(duckFactory);
     }
 
-    void simulate() {
-        Quackable mallardDuck = new QuackCounter(new MallardDuck());
-        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
-        Quackable duckCall = new QuackCounter(new DuckCall());
-        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+    void simulate(AbstractDuckFactory duckFactory) {
+        Quackable mallardDuck = duckFactory.createMallardDuck();
+        Quackable redheadDuck = duckFactory.createRedheadDuck();
+        Quackable duckCall = duckFactory.createDuckCall();
+        Quackable rubberDuck = duckFactory.createRubberDuck();
         Quackable gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("\nDuck Simulator");
+        System.out.println("\nDuck Simulator (+추상 팩토리)");
 
         simulate(mallardDuck);
         simulate(redheadDuck);
