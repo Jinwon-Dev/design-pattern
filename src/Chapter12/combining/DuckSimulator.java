0df62dfd7c41f1e4a91fbe1cc37c42438e29c3e4
Jinwon-Dev <yjw9424@naver.com>
2022-09-24 23:40:42 +0900

    컴포지트 패턴과 반복자 패턴을 적용하기

--- src/Chapter12/combining/DuckSimulator.java
+++ src/Chapter12/combining/DuckSimulator.java
@@ -9,19 +9,39 @@ public class DuckSimulator {
     }
 
     void simulate(AbstractDuckFactory duckFactory) {
-        Quackable mallardDuck = duckFactory.createMallardDuck();
         Quackable redheadDuck = duckFactory.createRedheadDuck();
         Quackable duckCall = duckFactory.createDuckCall();
         Quackable rubberDuck = duckFactory.createRubberDuck();
         Quackable gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("\nDuck Simulator (+추상 팩토리)");
+        System.out.println("\nDuck Simulator: 무리 (+컴포지트)");
 
-        simulate(mallardDuck);
-        simulate(redheadDuck);
-        simulate(duckCall);
-        simulate(rubberDuck);
-        simulate(gooseDuck);
+        Flock flockOfDucks = new Flock();
+
+        flockOfDucks.add(redheadDuck);
+        flockOfDucks.add(duckCall);
+        flockOfDucks.add(rubberDuck);
+        flockOfDucks.add(gooseDuck);
+
+        Flock flockOfMallards = new Flock();
+
+        Quackable mallardOne = duckFactory.createMallardDuck();
+        Quackable mallardTwo = duckFactory.createMallardDuck();
+        Quackable mallardThree = duckFactory.createMallardDuck();
+        Quackable mallardFour = duckFactory.createMallardDuck();
+
+        flockOfMallards.add(mallardOne);
+        flockOfMallards.add(mallardTwo);
+        flockOfMallards.add(mallardThree);
+        flockOfMallards.add(mallardFour);
+
+        flockOfDucks.add(flockOfMallards);
+
+        System.out.println("\nDuck Simulator: 전체 무리");
+        simulate(flockOfDucks);
+
+        System.out.println("\nDuck Simulator: 물오리 무리");
+        simulate(flockOfMallards);
 
         System.out.println("오리가 낸 횟수: " + QuackCounter.getQuacks() + " 번");
     }
